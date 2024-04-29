0332bd365997699da09cd505a366fd12f510e36e
MNitin-Reddy <mandanitinreddy@gmail.com>
2024-04-29 19:15:58 +0530

    commit

--- practice.py
+++ practice.py
@@ -1,6 +1,6 @@
 import os
 from random import randint
-for day in range(1, 4):
+for day in range(0, 1):
     for commits in range(0, randint(1, 4)):
         day = str(day)+' days ago'
         with open('file.txt', 'a')as file:
